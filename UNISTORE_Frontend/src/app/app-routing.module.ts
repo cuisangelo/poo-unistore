fb7eb6f753ebace4aa1667bf843b13283c28c7a6
LuisAngeloRP <luisangelorp26@gmail.com>
2022-07-21 17:24:03 -0500

    (ADD)
    Cambio de nombre frontend

--- UNISTORE_Frontend/src/app/app-routing.module.ts
+++ UNISTORE_Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TiendaComponent} from "./tienda/tienda.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"tienda", component:TiendaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- UNISTORE_Frontend/src/app/app.module.ts
+++ UNISTORE_Frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TiendaComponent } from './tienda/tienda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TiendaComponent
   ],
   imports: [
     BrowserModule,
