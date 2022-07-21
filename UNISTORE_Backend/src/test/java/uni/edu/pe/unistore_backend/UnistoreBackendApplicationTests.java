d99d4d9a3c263b2bbe06cb70a8019a3b6e63af19
LuisAngeloRP <luisangelorp26@gmail.com>
2022-07-21 17:51:26 -0500

    (ADD)
    xd

---
+++ UNISTORE_Backend/src/test/java/uni/edu/pe/unistore_backend/UnistoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.edu.pe.unistore_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnistoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
