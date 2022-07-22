a0882b639f5ff9f93f939953790953af9f2c72a0
SergioYupanqui <81380563+SergioYupanqui@users.noreply.github.com>
2022-07-22 15:01:17 -0500

    (ADD)

--- UNISTORE_Backend/src/main/java/uni/edu/pe/apprest/model/Cliente.java
+++ UNISTORE_Backend/src/main/java/uni/edu/pe/apprest/model/Cliente.java
@@ -8,11 +8,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Cliente {
-    private String id_cliente;
-    private String nombres ;
-    private String apellidos;
-    private String direccion ;
-    private String telefono ;
-    private String correo;
-    private String contrasena;
+    private String id_detalle;
+    private String id_cliente
+    private String id_producto VARCHAR(5),
+    private String descripcion VARCHAR(100),
+    cantidad_seleccionada NUMERIC(3),
+    precio_unitario NUMERIC (9, 2),
+    sub_precio NUMERIC (9, 2),
+    private String url_imagen VARCHAR(500),
 }
