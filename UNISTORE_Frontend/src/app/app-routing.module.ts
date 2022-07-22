c32c4f9ecd52f7d37fc195dfd78c9ed10d900437
LuisAngeloRP <luisangelorp26@gmail.com>
2022-07-22 01:09:43 -0500

    (ADD)

--- UNISTORE_Frontend/src/app/app-routing.module.ts
+++ UNISTORE_Frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {TiendaComponent} from "./tienda/tienda.component";
 import {LoginComponent} from "./login/login.component";
 
+
 const routes: Routes = [
   {path: "tienda", component: TiendaComponent},
   {path: "login", component: LoginComponent}
